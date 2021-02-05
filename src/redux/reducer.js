7ca6c18a213df786b74a60e7c3232329f7c5521f
arminv <arminv@users.noreply.github.com>
2021-02-05 18:39:15 -0500

    Progress (Part 3, mark 20:00)

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,10 @@
+import {
+  ADD_TODO,
+  UPDATE_FILTER,
+  UPDATE_TODO_STATUS,
+  CLEAR_COMPLETED,
+} from './actions';
+
 export const VisibilityFilters = {
   SHOW_ALL: 'All',
   SHOW_ACTIVE: 'Active',
@@ -11,6 +18,28 @@ const INITIAL_STATE = {
 
 export const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.todo],
+      };
+    case UPDATE_TODO_STATUS:
+      return {
+        ...state,
+        todos: todos.map((todo) => {
+          action.todo === todo ? { ...todo, complete: action.complete } : todo;
+        }),
+      };
+    case UPDATE_FILTER:
+      return {
+        ...state,
+        filter: action.filter,
+      };
+    case CLEAR_COMPLETED:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => !todo.complete),
+      };
     default:
       return state;
   }
