66c072f62e989745ff318101940574900094906d
arminv <arminv@users.noreply.github.com>
2021-02-08 00:19:35 -0500

    Removed unused import statement

--- src/views/base-view.js
+++ src/views/base-view.js
@@ -1,7 +1,5 @@
 import { LitElement } from '@polymer/lit-element';
 
-import { LitElement } from '@polymer/lit-element';
-
 export class BaseView extends LitElement {
   // NOTE: here we disable shadow DOM (hence the styles are NOT scoped anymore, etc.) and instead load into the 'light DOM':
   createRenderRoot() {
